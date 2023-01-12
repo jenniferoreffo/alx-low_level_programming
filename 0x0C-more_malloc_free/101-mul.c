1ce806c2d80662ebb01d521990a0c923038faf76
Jennifer Oreffo <57407703+jenniferoreffo@users.noreply.github.com>
2023-01-12 22:07:10 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - Program multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Description: Usage: mul num1 num2. num1 and num2 will be passed in base 10
- * Print the result, followed by a new line. 
- * If the number of arguments is incorrect, print Error, followed by a
- * new line, and exit with a status of 98
- * num1 and num2 should only be composed of digits. If not, print Error,
- * followed by a new line, and exit with a status of 98
- *
- * Return: int result
+ * main - check alx student code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Nothing
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	int num1, num2, mul;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		return (0);
+	}
 }
 	
