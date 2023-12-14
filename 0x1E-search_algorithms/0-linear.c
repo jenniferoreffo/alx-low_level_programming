e25e802922799585570f22a0998ea4a2bbdbf70e
Jennifer Oreffo <jennyoreffo@gmail.com>
2023-12-14 05:07:11 -0800

    commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,15 +10,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-	if (array == NULL || size <= 0)
-		return (-1);
+	int index = 0;
+	size_t i;
+
 	for (i = 0; i < size; i++)
         {
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
+		printf("Value checked array[%d] = [%d]\n", index, array[i]);
+		if (value == array[i])
+			return (index);
+	
 	return (-1);
+	}
 }
 
